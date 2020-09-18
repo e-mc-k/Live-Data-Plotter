777318fcfcba3056b12d62f8e8d402e789e28c1a
Evan Kias <evan.m.c.kias.com>
2020-09-18 15:37:32 -0500

    This version accomodates using only one transducer

--- Live-Data-Plotter_V1.py
+++ Live-Data-Plotter_V1.py
@@ -32,36 +32,59 @@ app.layout = html.Div(
             id='interval-component',
             interval=5000,  # in milliseconds
             n_intervals=0
-        )
-    ])
+            ),
+        dcc.RadioItems(
+            id='Num-Transducers',
+            options=[
+                {'label': '1 Transducer', 'value': '1'},
+                {'label': '2 Transducers', 'value': '2'}
+            ],
+            value='2',
+            labelStyle={'display': 'inline-block'}
+                       )
+            ])
 )
 
 
 @app.callback(Output('live-update-graph', 'figure'),
               [Input('interval-component', 'n_intervals'),
-               Input('full-filepath','value')],
-              )
-def liveupdate(n,filepath_):
+               Input('full-filepath','value'),
+               Input('Num-Transducers', 'value')]
+             )
+def liveupdate(n,filepath_,numTs):
     print(filepath_)
     if filepath_:
         df_dxd = pd.read_csv(filepath_)
         df_dxd = pd.read_csv(filepath_, skiprows=7, error_bad_lines=False)
-        df_dxd.drop(df_dxd.columns[[3, 4, 6,7]], axis=1, inplace=True)
-        df_dxd.columns = ['Date', 'Time', 'Outlet', 'Inlet']
-        df_dxd['DateTime'] = pd.to_datetime(df_dxd['Date'] + " " + df_dxd['Time'])
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df_dxd.DateTime, y=df_dxd.Outlet,
-                             name="Outlet"))
-        fig.add_trace(go.Scatter(x=df_dxd.DateTime, y=df_dxd.Inlet,
-                            name="Inlet", yaxis = "y2"))
-        # Set y-axes titles
-        fig.update_layout(yaxis=dict(title="Outlet", titlefont=dict(color="#1f77b4"), 
-                                     tickfont=dict(color="#1f77b4")),
-                           yaxis2=dict(title="Inlet",titlefont=dict(
-                               color="#ff7f0e"), tickfont=dict(color="#ff7f0e"),
-                               anchor="x", overlaying="y", side="right"),
-                           uirevision='foo'
-                           )                          
+        if numTs == '2':
+            df_dxd.drop(df_dxd.columns[[3, 4, 6,7]], axis=1, inplace=True)
+            df_dxd.columns = ['Date', 'Time', 'Outlet', 'Inlet']
+            df_dxd['DateTime'] = pd.to_datetime(df_dxd['Date'] + " " + df_dxd['Time'])
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=df_dxd.DateTime, y=df_dxd.Outlet,
+                                 name="Outlet"))
+            fig.add_trace(go.Scatter(x=df_dxd.DateTime, y=df_dxd.Inlet,
+                                name="Inlet", yaxis = "y2"))
+            # Set y-axes titles
+            fig.update_layout(yaxis=dict(title="Outlet", titlefont=dict(color="#1f77b4"), 
+                                         tickfont=dict(color="#1f77b4")),
+                               yaxis2=dict(title="Inlet",titlefont=dict(
+                                   color="#ff7f0e"), tickfont=dict(color="#ff7f0e"),
+                                   anchor="x", overlaying="y", side="right"),
+                               uirevision='foo'
+                               )
+        if numTs == '1':
+            df_dxd.drop(df_dxd.columns[[3]], axis=1, inplace=True)
+            df_dxd.columns = ['Date', 'Time', 'Pressure']
+            df_dxd['DateTime'] = pd.to_datetime(df_dxd['Date'] + " " + df_dxd['Time'])
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=df_dxd.DateTime, y=df_dxd.Pressure,
+                                 name="Pressure"))
+            # Set y-axes titles
+            fig.update_layout(yaxis=dict(title="Pressure", titlefont=dict(color="#1f77b4"), 
+                                         tickfont=dict(color="#1f77b4")),
+                               uirevision='foo'
+                               )                           
         return fig
     else:
         # fig = px.scatter(x=[1], y=[1])
@@ -70,4 +93,4 @@ def liveupdate(n,filepath_):
 
 if __name__ == '__main__':
     # app.run_server(debug=True)
-    app.run_server(debug=True,port=8100,host='10.0.101.7')
+    app.run_server(debug=True,port=8105,host='10.0.100.140')
